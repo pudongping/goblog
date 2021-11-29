8a665c0965a8d670a86e642b21a3751a3684ee59
pudongping <276558492@qq.com>
2021-11-30 00:00:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是333 goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:tt@example.com\">tt@example.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
@@ -19,7 +16,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:tt@example.com\">tt@example.com</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":3000", nil)
 }
