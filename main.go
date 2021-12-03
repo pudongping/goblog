90ed856be02b68d40fe2cc61695a6234c0f6f50b
pudongping <276558492@qq.com>
2021-12-03 23:22:10 +0800

    adjust

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request)  {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>hello, 我是 alex</h1>")
 }
@@ -18,16 +18,16 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:tt@example.com\">tt@example.com</a>")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request)  {
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
-func articlesIndexHandler(w http.ResponseWriter, r *http.Request)  {
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Fprint(w, "文章的 id 为：" + id)
+	fmt.Fprint(w, "文章的 id 为："+id)
 }
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +40,6 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
-
 
 func main() {
 
