bc62b0b74c4e7c4c83822992bab225df4fede8f4
pudongping <276558492@qq.com>
2021-12-06 11:12:35 +0800

    表单验证

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
@@ -56,21 +59,88 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, html, storeURL)
 }
 
+// ArticlesFormData 创建博文表单数据，用于给模版文件传输变量时使用
+type ArticlesFormData struct {
+	Title, Body string
+	URL *url.URL
+	Errors map[string]string
+}
+
 // 创建博文时，提交数据
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
-	// 从请求中解析请求参数
-	err := r.ParseForm()
-	if err != nil {
-		// 解析错误
-		fmt.Fprint(w, "请提供正确的数据！")
-		return
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+
+	errors := make(map[string]string)
+	titleLen := utf8.RuneCountInString(title)  // 计算 title 的长度
+	bodyLen := utf8.RuneCountInString(body)
+
+	// 验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if titleLen < 3 || titleLen > 40 {
+		errors["title"] = "标题长度需介于 3-40"
 	}
 
-	title := r.PostForm.Get("title")
-	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
-	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
-	fmt.Fprintf(w, "title 的值为: %v", title)
+	// 验证内容
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if bodyLen < 10 {
+		errors["body"] = "内容长度需要大于或等于 10 个字节"
+	}
+
+	// 检查是否含有错误
+	if len(errors) == 0 {
+		fmt.Fprint(w, "验证通过！<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		fmt.Fprintf(w, "title 的长度为: %v <br>", titleLen)
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", bodyLen)
+	} else {
+
+		html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+    <style type="text/css">.error {color: red;}</style>
+</head>
+<body>
+    <form action="{{ .URL }}" method="post">
+        <p><input type="text" name="title" value="{{ .Title }}"></p>
+        {{ with .Errors.title }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+        {{ with .Errors.body }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+		storeURL, _ := router.Get("articles.store").URL()
+
+		// 构建 ArticlesFormData 里的数据，storeURL 是通过路由参数生成的 URL 路径
+		data := ArticlesFormData{
+			Title:  title,
+			Body:   body,
+			URL:    storeURL,
+			Errors: errors,
+		}
+		tmpl, err := template.New("create-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 
 }
 
