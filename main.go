6f3c5832c7c12f1c8a049fa45b91c4d1ffaa18ae
pudongping <276558492@qq.com>
2021-12-13 09:17:39 +0800

    优化代码

--- main.go
+++ main.go
@@ -97,14 +97,10 @@ type Article struct {
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
-	vars := mux.Vars(r)
-	id := vars["id"]
+	id := getRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
-	article := Article{}
-	query := "select * from articles where id = ?"
-	// 传参应与数据表字段的顺序保持一致
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+	article, err := getArticleByID(id)
 
 	// 3. 如果出现错误
 	if err != nil {
@@ -219,17 +215,28 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// getRouteVariable 获取路由参数
+func getRouteVariable(parameterName string, r *http.Request) string {
+	vars := mux.Vars(r)
+	return vars[parameterName]
+}
+
+// getArticleByID 通过文章 id 获取文章
+func getArticleByID(id string) (Article, error) {
+	article := Article{}
+	query := "SELECT * FROM articles WHERE id = ?"
+	// 传参应与数据表字段的顺序保持一致
+	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+	return article, err
+}
+
 func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1. 获取 URL 参数
-	vars := mux.Vars(r)
-	id := vars["id"]
+	id := getRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
-	article := Article{}
-	query := "select * from articles where id = ?"
-	// 传参应与数据表字段的顺序保持一致
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+	article, err := getArticleByID(id)
 
 	// 3. 如果出现错误
 	if err != nil {
