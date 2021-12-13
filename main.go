dd9482b023fbc2de84271244932fd43909d7ebad
pudongping <276558492@qq.com>
2021-12-13 09:53:25 +0800

    更新文章

--- main.go
+++ main.go
@@ -270,7 +270,93 @@ func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "更新文章")
+	// 1. 获取 URL 参数
+	id := getRouteVariable("id", r)
+
+	// 2. 读取对应的文章数据
+	_, err := getArticleByID(id)
+
+	// 3. 如果出现错误
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// 3.1 数据未找到
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 文章未找到")
+		} else {
+			// 3.2 数据库错误
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+	} else {
+		// 4. 未出现错误
+
+		// 4.1 表单验证
+		title := r.PostFormValue("title")
+		body := r.PostFormValue("body")
+
+		errors := make(map[string]string)
+
+		titleLen := utf8.RuneCountInString(title) // 计算 title 的长度
+		bodyLen := utf8.RuneCountInString(body)
+
+		// 验证标题
+		if title == "" {
+			errors["title"] = "标题不能为空"
+		} else if titleLen < 3 || titleLen > 40 {
+			errors["title"] = "标题长度需介于 3-40"
+		}
+
+		// 验证内容
+		if body == "" {
+			errors["body"] = "内容不能为空"
+		} else if bodyLen < 10 {
+			errors["body"] = "内容长度需要大于或等于 10 个字节"
+		}
+
+		if len(errors) == 0 {
+
+			// 4.2 表单验证通过，更新数据
+			query := "update articles set title = ?, body = ? where id = ?"
+			rs, err := db.Exec(query, title, body, id)
+
+			if err != nil {
+				checkError(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, "500 服务器内部错误")
+			}
+
+			// 更新成功，跳转到文章详情页
+			// 通过返回的影响行数来判断是否更新成功
+			if n, _ := rs.RowsAffected(); n > 0 {
+				showURL, _ := router.Get("articles.show").URL("id", id)
+				// 跳转到文章详情页
+				http.Redirect(w, r, showURL.String(), http.StatusFound)
+			} else {
+				fmt.Fprint(w, "您没有做任何更改！")
+			}
+
+		} else {
+
+			// 4.3 表单验证不通过，显示理由
+
+			updateURL, _ := router.Get("articles.update").URL("id", id)
+			data := ArticlesFormData{
+				Title:  title,
+				Body:   body,
+				URL:    updateURL,
+				Errors: errors,
+			}
+			tmpl, err := template.ParseFiles("resources/views/articles/edit.gohtml")
+			checkError(err)
+
+			err = tmpl.Execute(w, data)
+			checkError(err)
+
+		}
+
+	}
+
 }
 
 func saveArticleToDB(title, body string) (int64, error) {
