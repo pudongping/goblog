9d77ee0a8ac519e3ad9cf0791584005e04ecd162
pudongping <276558492@qq.com>
2021-12-16 15:42:00 +0800

    重构 GetRouteVariable

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func Int64ToString(num int64) string {
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := getArticleByID(id)
@@ -291,12 +291,6 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// getRouteVariable 获取路由参数
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
 // getArticleByID 通过文章 id 获取文章
 func getArticleByID(id string) (Article, error) {
 	article := Article{}
@@ -309,7 +303,7 @@ func getArticleByID(id string) (Article, error) {
 func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1. 获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := getArticleByID(id)
@@ -347,7 +341,7 @@ func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	_, err := getArticleByID(id)
@@ -421,7 +415,7 @@ func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1. 获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := getArticleByID(id)
--- pkg/route/router.go
+++ pkg/route/router.go
@@ -1,6 +1,8 @@
 package route
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -21,4 +23,10 @@ func Name2URL(routeName string, pairs ...string) string {
 	}
 
 	return url.String()
+}
+
+// GetRouteVariable 获取 URI 路由参数
+func GetRouteVariable(parameterName string, r *http.Request) string {
+	vars := mux.Vars(r)
+	return vars[parameterName]
 }
