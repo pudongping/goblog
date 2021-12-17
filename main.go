7d706f1fb6739c00d74d37c62c015b033c470c82
pudongping <276558492@qq.com>
2021-12-18 04:38:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -12,88 +11,10 @@ import (
 
 	"github.com/pudongping/goblog/bootstrap"
 	"github.com/pudongping/goblog/pkg/database"
-	"github.com/pudongping/goblog/pkg/logger"
 )
 
 var router *mux.Router
 var db *sql.DB
-
-// Article 对应一条文章数据
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-func (a Article) Delete() (rowsAffected int64, err error) {
-	rs, err := db.Exec("delete from articles where id = " + strconv.FormatInt(a.ID, 10))
-
-	if err != nil {
-		return 0, err
-	}
-
-	// 删除成功，跳转到文章详情页
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-
-	return 0, nil
-}
-
-// getArticleByID 通过文章 id 获取文章
-func getArticleByID(id string) (Article, error) {
-	article := Article{}
-	query := "SELECT * FROM articles WHERE id = ?"
-	// 传参应与数据表字段的顺序保持一致
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
-	return article, err
-}
-
-func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
-
-	// 1. 获取 URL 参数
-	id := getRouteVariable("id", r)
-
-	// 2. 读取对应的文章数据
-	article, err := getArticleByID(id)
-
-	// 3. 如果出现错误
-	if err != nil {
-		if err == sql.ErrNoRows {
-			// 3.1 数据未找到
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "404 文章未找到")
-		} else {
-			// 3.2 数据库错误
-			logger.LogError(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "500 服务器内部错误")
-		}
-	} else {
-		// 4. 未出现错误，执行删除操作
-		rowsAffected, err := article.Delete()
-
-		// 4.1 发生错误
-		if err != nil {
-			// 应该是 sql 报错了
-			logger.LogError(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "500 服务器内部错误")
-		} else {
-			// 4.2 未发生错误
-			if rowsAffected > 0 {
-				// 重定向到文章列表页
-				indexURL, _ := router.Get("articles.index").URL()
-				http.Redirect(w, r, indexURL.String(), http.StatusFound)
-			} else {
-				// Edge case
-				w.WriteHeader(http.StatusNotFound)
-				fmt.Fprint(w, "404 文章未找到")
-			}
-		}
-
-	}
-
-}
 
 // 强制内容类型为 HTML 的中间件
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -117,12 +38,6 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
-// getRouteVariable 获取 URI 路由参数
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
 func main() {
 
 	database.Initialize()
@@ -131,10 +46,6 @@ func main() {
 	// 初始化 gorm
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
-
-	// 在 Gorilla Mux 中，如果未指定请求方法，默认会匹配所有方法
-	// 删除文章
-	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 
 	// 中间件：强制内容类型为 HTML
 	router.Use(forceHTMLMiddleware)
