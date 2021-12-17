67bb7343db455e538a31985349aea9887a9c5f64
pudongping <276558492@qq.com>
2021-12-18 04:52:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,39 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 
+	"github.com/pudongping/goblog/app/http/middlewares"
 	"github.com/pudongping/goblog/bootstrap"
 	"github.com/pudongping/goblog/pkg/database"
+	"github.com/pudongping/goblog/pkg/logger"
 )
 
 var router *mux.Router
 var db *sql.DB
-
-// 强制内容类型为 HTML 的中间件
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-// removeTrailingSlash 去掉 path 后面的 `/`
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 除首页以外，移除所有请求路径后面的斜线
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		// 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 
@@ -47,15 +26,10 @@ func main() {
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
 	// 通过命名路由获取 URL 示例
 	homeURL, _ := router.Get("home").URL()
 	fmt.Println("homeURL: ", homeURL)
 
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
