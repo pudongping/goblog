34a86fb832cdbfee5ea07efee310ee6172d94bf7
pudongping <276558492@qq.com>
2021-12-18 03:04:32 +0800

    重构文章列表

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -1,8 +1,20 @@
 package article
 
+import (
+	"github.com/pudongping/goblog/app/models"
+	"github.com/pudongping/goblog/pkg/route"
+)
+
 // Article 文章模型
 type Article struct {
+	models.BaseModel
+
 	ID    uint64
 	Title string
 	Body  string
+}
+
+// Link 方法用来生成文章链接
+func (article Article) Link() string {
+	return route.Name2URL("articles.show", "id", article.GetStringID())
 }
