d9542cd57ea5d196b6b03dcabbcba101139f2130
pudongping <276558492@qq.com>
2021-12-18 03:04:56 +0800

    Update web.go

--- routes/web.go
+++ routes/web.go
@@ -19,6 +19,9 @@ func RegisterWebRoutes(r *mux.Router) {
 
 	// 文章相关页面
 	ac := new(controllers.ArticlesController)
+	// 文章详情
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
+	// 文章列表
+	r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
 
 }
