c2b2036423356fbaedf7035249e6f5fac3052968
pudongping <276558492@qq.com>
2021-12-19 04:15:45 +0800

    单独封装请求验证类

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -1,12 +1,11 @@
 package controllers
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/thedevsaddam/govalidator"
-
+	"github.com/pudongping/goblog/app/models/user"
+	"github.com/pudongping/goblog/app/requests"
 	"github.com/pudongping/goblog/pkg/view"
 )
 
@@ -18,14 +17,6 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 	view.RenderSimple(w, view.D{}, "auth.register")
 }
 
-// 验证器
-type userForm struct {
-	Name            string `valid:"name"`
-	Email           string `valid:"email"`
-	Password        string `valid:"password"`
-	PasswordConfirm string `valid:"password_confirm"`
-}
-
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 	// 0. 初始化变量
@@ -35,7 +26,7 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 	passwordConfirm := r.PostFormValue("password_confirm")
 
 	// 1. 初始化数据
-	_user := userForm{
+	_user := user.User{
 		Name:            name,
 		Email:           email,
 		Password:        password,
@@ -43,37 +34,30 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// 2. 表单规则
-	rules := govalidator.MapData{
-		"name":             []string{"required", "alpha_num", "between:3,20"},
-		"email":            []string{"required", "min:4", "max:30", "email"},
-		"password":         []string{"required", "min:6"},
-		"password_confirm": []string{"required"},
-	}
-
-	// 3. 配置选项
-	opts := govalidator.Options{
-		Data:          &_user,
-		Rules:         rules,
-		TagIdentifier: "valid", // Struct 标签标识符
-	}
-
-	// 4. 开始验证
-	errs := govalidator.New(opts).ValidateStruct()
+	errs := requests.ValidateRegistrationForm(_user)
 
 	if len(errs) > 0 {
-		// 4.1 有错误发生，打印错误数据
+		// 3.1 有错误发生，打印错误数据
 		// json.MarshalIndent 用来将 Go 对象格式成为 json 字符串，并加上合理的缩进
-		data, _ := json.MarshalIndent(errs, "", "  ")
-		fmt.Fprint(w, string(data))
+		// data, _ := json.MarshalIndent(errs, "", "  ")
+		// fmt.Fprint(w, string(data))
+
+		// 3. 表单不通过，重新显示表单
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User": _user,
+		}, "auth.register")
+
 	} else {
-		// _user.Create()
-		//
-		// if _user.ID > 0 {
-		// 	fmt.Fprint(w, "插入成功，ID为"+_user.GetStringID())
-		// } else {
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	fmt.Fprint(w, "创建用户失败，请联系管理员")
-		// }
+		// 4. 验证成功，创建数据
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
 	}
 
 	// 3. 表单不通过，则重新显示表单
