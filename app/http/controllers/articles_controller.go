cb0da130ef558ec0ea4bde6888abbe55c74f5891
pudongping <276558492@qq.com>
2021-12-19 01:07:04 +0800

    模型基类和 view 包

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"path/filepath"
 	"strconv"
 	"unicode/utf8"
 
@@ -13,6 +12,7 @@ import (
 	"github.com/pudongping/goblog/app/models/article"
 	"github.com/pudongping/goblog/pkg/logger"
 	"github.com/pudongping/goblog/pkg/route"
+	"github.com/pudongping/goblog/pkg/view"
 )
 
 // ArticlesController 文章相关页面
@@ -32,25 +32,7 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "500 服务器内部错误")
 	} else {
 		// 2. 加载模版
-
-		// 2.0 设置模版相对路径
-		viewDir := "resources/views"
-
-		// 2.1 所有布局模板文件 Slice
-		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
-		logger.LogError(err)
-
-		// 2.2 在 Slice 里新增我们的目标文件
-		newFiles := append(files, viewDir+"/articles/index.gohtml")
-
-		// 2.3 解析模板文件
-		tmpl, err := template.ParseFiles(newFiles...)
-		logger.LogError(err)
-
-		// 2.4 渲染模板，将所有文章的数据传输进去
-		// myapp 是模板的名称（这是模版关键词 define 定义的模版名称，不是模版文件名称）
-		err = tmpl.ExecuteTemplate(w, "myapp", articles)
-		logger.LogError(err)
+		view.Render(w, "articles.index", articles)
 	}
 
 }
@@ -77,28 +59,7 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 		// 4. 读取成功，显示文章
-
-		// 4.0 设置模板相对路径
-		viewDir := "resources/views"
-
-		// 4.1 所有布局模板文件 Slice
-		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
-		logger.LogError(err)
-
-		// 4.2 在 Slice 里新增我们的目标文件
-		newFiles := append(files, viewDir+"/articles/show.gohtml")
-
-		// 4.3 解析模板文件
-		tmpl, err := template.New("show.gohtml").
-			Funcs(template.FuncMap{
-				"RouteName2URL":  route.Name2URL,
-			}).
-			ParseFiles(newFiles...)
-		logger.LogError(err)
-
-		// 4.4 渲染模板，将所有文章的数据传输进去
-		err = tmpl.ExecuteTemplate(w, "myapp", article)
-		logger.LogError(err)
+		view.Render(w, "articles.show", article)
 	}
 
 }
