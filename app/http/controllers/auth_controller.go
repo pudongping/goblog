e8409a5d2054f97376e700eb57bd297f6514b083
pudongping <276558492@qq.com>
2021-12-19 02:23:46 +0800

    Create auth_controller.go

---
+++ app/http/controllers/auth_controller.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/pudongping/goblog/pkg/view"
+)
+
+type AuthController struct {
+}
+
+// Register 注册页面
+func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
+	view.RenderSimple(w, view.D{}, "auth.register")
+}
+
+// DoRegister 处理注册逻辑
+func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
+	//
+}
