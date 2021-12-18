d443b81651d955dfb6c5afe4229459597070df17
pudongping <276558492@qq.com>
2021-12-19 03:14:25 +0800

    Update model.go

--- app/models/model.go
+++ app/models/model.go
@@ -1,14 +1,16 @@
 package models
 
 import (
+	"time"
+
 	"github.com/pudongping/goblog/pkg/types"
 )
 
 type BaseModel struct {
 	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;not null"`
 
-	// CreatedAt time.Time `gorm:"column:created_at;index"`
-	// UpdatedAt time.Time `gorm:"column:updated_at;index"`
+	CreatedAt time.Time `gorm:"column:created_at;index"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index"`
 
 	// 支持 gorm 软删除
 	// DeletedAt gorm.DeletedAt `gorm:"column:deleted_at" sql:"index"`
