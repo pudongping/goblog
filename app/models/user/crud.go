93339ce58f07761e072ab63cfacc8f620b671233
pudongping <276558492@qq.com>
2021-12-20 00:16:06 +0800

    Update crud.go

--- app/models/user/crud.go
+++ app/models/user/crud.go
@@ -3,6 +3,7 @@ package user
 import (
 	"github.com/pudongping/goblog/pkg/logger"
 	"github.com/pudongping/goblog/pkg/model"
+	"github.com/pudongping/goblog/pkg/types"
 )
 
 // Create 创建用户，通过 User.ID 来判断是否创建成功
@@ -13,4 +14,26 @@ func (user *User) Create() (err error) {
 	}
 
 	return nil
+}
+
+// Get 通过 ID 获取用户
+func Get(idstr string) (User, error) {
+	var user User
+	id := types.StringToUint64(idstr)
+	if err := model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+// GetByEmail 通过 email 来获取用户
+func GetByEmail(email string) (User, error) {
+	var user User
+	err := model.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
