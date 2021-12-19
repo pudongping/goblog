0b472c4f401d2ba63a201e3aa7e6fa05f2633522
pudongping <276558492@qq.com>
2021-12-20 00:21:07 +0800

    更换首页路由

--- routes/web.go
+++ routes/web.go
@@ -15,11 +15,12 @@ func RegisterWebRoutes(r *mux.Router) {
 	// 静态页面
 	pc := new(controllers.PagesController)
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
-	r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
 
 	// 文章相关页面
 	ac := new(controllers.ArticlesController)
+	// 首页
+	r.HandleFunc("/", ac.Index).Methods("GET").Name("home")
 	// 文章详情
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 	// 文章列表
