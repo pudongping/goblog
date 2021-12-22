63fcc8cce57bb9e8cd7119e25817917c44c794bc
pudongping <276558492@qq.com>
2021-12-22 23:37:02 +0800

    Update flash.go

--- pkg/flash/flash.go
+++ pkg/flash/flash.go
@@ -15,6 +15,8 @@ var flashKey = "_flashes"
 func init() {
 	// 在 gorilla/sessions 中存储 map 和 struct 数据需
 	// 要提前注册 gob，方便后续 gob 序列化编码、解码
+	// 标准库 gob 是 Go 专属的编解码方式，是标准库自带的一个数据结构序列化的编码 / 解码工具。
+	// 类似于 JSON 或 XML，不过执行效率比他们更高。特别适合在 Go 语言程序间传递数据
 	gob.Register(Flashes{})
 }
 
