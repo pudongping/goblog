f2fcd2017ab7036690cecabfd075ba12a8c67de9
pudongping <276558492@qq.com>
2021-12-23 02:19:54 +0800

    写配置信息

--- main.go
+++ main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-
 	"github.com/pudongping/goblog/app/http/middlewares"
 	"github.com/pudongping/goblog/bootstrap"
-	"github.com/pudongping/goblog/pkg/database"
+	"github.com/pudongping/goblog/config"
+	c "github.com/pudongping/goblog/pkg/config"
 	"github.com/pudongping/goblog/pkg/logger"
 )
 
 var router *mux.Router
-var db *sql.DB
+
+//var db *sql.DB
+
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
 
 func main() {
 
-	database.Initialize()
-	db = database.DB
+	//database.Initialize()
+	//db = database.DB
 
 	// 初始化 gorm
 	bootstrap.SetupDB()
+	// 初始化路由绑定
 	router = bootstrap.SetupRoute()
 
 	// 通过命名路由获取 URL 示例
 	homeURL, _ := router.Get("home").URL()
 	fmt.Println("homeURL: ", homeURL)
 
-	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	err := http.ListenAndServe(fmt.Sprintf(":%s", c.GetString("app.port")), middlewares.RemoveTrailingSlash(router))
 	logger.LogError(err)
+
 }
