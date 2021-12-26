bb9d2346886df38d9f6ef4f1fe17d36377f70cfb
pudongping <276558492@qq.com>
2021-12-26 10:27:30 +0800

    Update article.go

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -13,6 +13,7 @@ type Article struct {
 	Title string `gorm:"type:varchar(255);not null;" valid:"title"`
 	Body  string `gorm:"type:longtext;not null;" valid:"body"`
 	UserID uint64 `gorm:"not null;index"`
+	CategoryID uint64 `gorm:"not null;default:0;index"`
 	User user.User  // 实现模型关联
 }
 
